1dbf9fa9d825758d2018bbd387780b5cb9e8107d
Joseph Trubenstein <josephtrubenstein@gmail.com>
2023-03-18 15:13:32 +0900

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,24 +6,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <div>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p>
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
+
     </div>
-    <ul>
-    </ul>
+
   </Layout>
 )
 
