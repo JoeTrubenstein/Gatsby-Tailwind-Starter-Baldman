a7f0734ac3dabc4c5c4dd08f54f8ce8cff91df65
Joseph Trubenstein <josephtrubenstein@gmail.com>
2023-05-24 15:05:26 +0900

    layout

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const Header = ({ siteTitle }) => (
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{siteTitle}</h1>
           <p className="mb-8 leading-relaxed">I really like Gatsby and Tailwind. This starter template and its components lets me, or anyone, spin up a new project quickly.</p>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg">Button</button>
+            <button className="inline-flex text-white bg-baldcoral border-0 py-2 px-6 focus:outline-none hover:bg-baldcoral rounded text-lg">Button</button>
             <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
           </div>
         </div>
